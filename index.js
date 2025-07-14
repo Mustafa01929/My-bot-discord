99960f1226e590f02a96bae26019a5a01a5c8083
Mustafa01929 <mosafabank@gmail.com>
2025-07-14 21:49:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3521,4 +3521,4 @@ function readJsons() {
 function writeJsons(data) {
   fs.writeFileSync(applys, JSON.stringify(data, null, 2));
 }
-client.login(process.env.TOKEN);
+client.login("MTEzNjg3NDExMzEyMzEzMTMyMzEz.L-123456Abc.DEFghijkLmnoPqrstuVwxyz123");
